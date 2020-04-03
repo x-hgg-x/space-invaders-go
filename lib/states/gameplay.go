d5854f5323e476f5aafb3015b1f64df8ad299d92
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2020-04-03 16:00:23 +0200

    Add pause menu

--- lib/states/gameplay.go
+++ lib/states/gameplay.go
@@ -1,15 +1,20 @@
 package states
 
 import (
+	ecs "github.com/x-hgg-x/goecs"
+	ec "github.com/x-hgg-x/goecsengine/components"
 	"github.com/x-hgg-x/goecsengine/loader"
 	"github.com/x-hgg-x/goecsengine/states"
 	w "github.com/x-hgg-x/goecsengine/world"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
 // GameplayState is the main game state
-type GameplayState struct{}
+type GameplayState struct {
+	runningAnimations []*ec.AnimationControl
+}
 
 // OnStart method
 func (st *GameplayState) OnStart(world w.World) {
@@ -25,11 +30,27 @@ func (st *GameplayState) OnStart(world w.World) {
 
 // OnPause method
 func (st *GameplayState) OnPause(world w.World) {
+	// Pause running animations
+	st.runningAnimations = []*ec.AnimationControl{}
+	world.Manager.Join(world.Components.Engine.AnimationControl).Visit(ecs.Visit(func(entity ecs.Entity) {
+		animationControl := world.Components.Engine.AnimationControl.Get(entity).(*ec.AnimationControl)
+		if animationControl.GetState().Type == ec.ControlStateRunning {
+			animationControl.Command.Type = ec.AnimationCommandPause
+			st.runningAnimations = append(st.runningAnimations, animationControl)
+		}
+	}))
+
 	ebiten.SetCursorMode(ebiten.CursorModeVisible)
 }
 
 // OnResume method
 func (st *GameplayState) OnResume(world w.World) {
+	// Resume running animations
+	for _, animationControl := range st.runningAnimations {
+		animationControl.Command.Type = ec.AnimationCommandStart
+	}
+	st.runningAnimations = []*ec.AnimationControl{}
+
 	ebiten.SetCursorMode(ebiten.CursorModeHidden)
 }
 
@@ -42,5 +63,8 @@ func (st *GameplayState) OnStop(world w.World) {
 
 // Update method
 func (st *GameplayState) Update(world w.World, screen *ebiten.Image) states.Transition {
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return states.Transition{Type: states.TransPush, NewStates: []states.State{&PauseMenuState{}}}
+	}
 	return states.Transition{}
 }
---
+++ lib/states/pause_menu.go
@@ -0,0 +1,82 @@
+package states
+
+import (
+	"fmt"
+
+	ecs "github.com/x-hgg-x/goecs"
+	"github.com/x-hgg-x/goecsengine/loader"
+	"github.com/x-hgg-x/goecsengine/states"
+	w "github.com/x-hgg-x/goecsengine/world"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/inpututil"
+)
+
+// PauseMenuState is the pause menu state
+type PauseMenuState struct {
+	pauseMenu []ecs.Entity
+	selection int
+}
+
+//
+// Menu interface
+//
+
+func (st *PauseMenuState) getSelection() int {
+	return st.selection
+}
+
+func (st *PauseMenuState) setSelection(selection int) {
+	st.selection = selection
+}
+
+func (st *PauseMenuState) confirmSelection() states.Transition {
+	switch st.selection {
+	case 0:
+		// Resume
+		return states.Transition{Type: states.TransPop}
+	case 1:
+		// Main Menu
+		return states.Transition{Type: states.TransReplace, NewStates: []states.State{&MainMenuState{}}}
+	case 2:
+		// Exit
+		return states.Transition{Type: states.TransQuit}
+	}
+	panic(fmt.Errorf("unknown selection: %d", st.selection))
+}
+
+func (st *PauseMenuState) getMenuIDs() []string {
+	return []string{"resume", "main_menu", "exit"}
+}
+
+func (st *PauseMenuState) getCursorMenuIDs() []string {
+	return []string{"cursor_resume", "cursor_main_menu", "cursor_exit"}
+}
+
+//
+// State interface
+//
+
+// OnPause method
+func (st *PauseMenuState) OnPause(world w.World) {}
+
+// OnResume method
+func (st *PauseMenuState) OnResume(world w.World) {}
+
+// OnStart method
+func (st *PauseMenuState) OnStart(world w.World) {
+	st.pauseMenu = loader.LoadEntities("assets/metadata/entities/ui/pause_menu.toml", world, nil)
+}
+
+// OnStop method
+func (st *PauseMenuState) OnStop(world w.World) {
+	world.Manager.DeleteEntities(st.pauseMenu...)
+}
+
+// Update method
+func (st *PauseMenuState) Update(world w.World, screen *ebiten.Image) states.Transition {
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return states.Transition{Type: states.TransPop}
+	}
+	return updateMenu(st, world)
+}
